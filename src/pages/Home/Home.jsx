fa20f46d2ddb02e23c7b09c7e00362343a483e68
Anonymous <suportdevs@gmail.com>
2023-09-30 00:40:12 +0600

    featured info designed

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import "./Home.css";
 import FeaturedInfo from "../../components/FeaturedInfo/FeaturedInfo";
+import Chart from "../../components/Chart/Chart";
 
 export default function Home(){
     return (
         <div className="home">
             <FeaturedInfo />
+            <Chart />
         </div>
     )
 }
---
+++ src/utilies/userData.js
@@ -0,0 +1,50 @@
+export const data = [
+    {
+      name: 'Jan',
+      "Active User": 4000,
+    },
+    {
+      name: 'Feb',
+      "Active User": 1000,
+    },
+    {
+      name: 'Mar',
+      "Active User": 4000,
+    },
+    {
+      name: 'Apr',
+      "Active User": 4000,
+    },
+    {
+      name: 'May',
+      "Active User": 2000,
+    },
+    {
+      name: 'Jun',
+      "Active User": 4000,
+    },
+    {
+      name: 'Jul',
+      "Active User": 5000,
+    },
+    {
+      name: 'Sep',
+      "Active User": 4000,
+    },
+    {
+      name: 'Oct',
+      "Active User": 6000,
+    },
+    {
+      name: 'Agu',
+      "Active User": 4000,
+    },
+    {
+      name: 'Nov',
+      "Active User": 4000,
+    },
+    {
+      name: 'Dec',
+      "Active User": 4000,
+    },
+  ];
