7169ef943e85cc52916a03f44dbff5ac60291f16
Anonymous <suportdevs@gmail.com>
2023-10-02 20:44:15 +0600

    new user component designed

--- src/pages/NewUser/NewUser.jsx
+++ src/pages/NewUser/NewUser.jsx
@@ -1,18 +1,10 @@
 import "./NewUser.css";
-import { CalendarToday, FmdGoodOutlined, MailOutline, PermIdentity, PhoneAndroid, PhotoCameraOutlined } from "@mui/icons-material";
 
 export default function NewUser(){
     return (
         <div className="newUser">
             <h3 className="newUserTitle">New User</h3>
-            <form >
-                {/* <div className="newUserAvatar">
-                    <label htmlFor="avatar" className="userEidtRightAvatarlabel">
-                    <img className="newUserImg" src="https://images.pexels.com/photos/943235/pexels-photo-943235.jpeg?auto=compress&cs=tinysrgb&w=600" alt="" />
-                    <span className="newUserAvatarMask"><PhotoCameraOutlined /></span>
-                    <input className="newUserItemInput" type="file" name="avatar" id="avatar" style={{display: 'none'}} />
-                    </label>
-                </div> */}
+            <form className="newUserForm">
                 <div className="newUserItem">
                     <label htmlFor="">Name</label>
                     <input className="newUserItemInput" type="text" name="name" placeholder="Name" />
@@ -22,12 +14,34 @@ export default function NewUser(){
                     <input className="newUserItemInput" type="email" name="email" placeholder="Email" />
                 </div>
                 <div className="newUserItem">
+                    <label htmlFor="">Password</label>
+                    <input className="newUserItemInput" type="password" name="password" placeholder="Password" />
+                </div>
+                <div className="newUserItem">
                     <label htmlFor="">Phone</label>
                     <input className="newUserItemInput" type="text" name="phone" placeholder="Phone" />
                 </div>
                 <div className="newUserItem">
                     <label htmlFor="">Address</label>
                     <input className="newUserItemInput" type="text" name="address" placeholder="Address" />
+                </div>
+                <div className="newUserItem">
+                    <label htmlFor="">Gender</label>
+                    <div className="newUserInputGroup">
+                    <input className="newUserItemInput" type="radio" name="gender" value="male" />
+                    <label htmlFor="gender">Male</label>
+                    <input className="newUserItemInput" type="radio" name="gender" value="female" />
+                    <label htmlFor="gender">Female</label>
+                    <input className="newUserItemInput" type="radio" name="gender" value="other" />
+                    <label htmlFor="gender">Other</label>
+                    </div>
+                </div>
+                <div className="newUserItem">
+                    <label htmlFor="">Active</label>
+                    <select className="newUserItemInput" name="active">
+                        <option value="Yes">Yes</option>
+                        <option value="No">No</option>
+                    </select>
                 </div>
                 <div className="newUserItem">
                     <button className="newUserItemBtn">Update</button>
